71e13449fe3f91970ac115cee0f5259b0625238e
Chloé ERNDT <chloerndt@gmail.com>
2024-01-04 17:48:10 +0100

    fix css to have a default dark mode

--- mealplan/static/mealplan/js/recipe.js
+++ mealplan/static/mealplan/js/recipe.js
@@ -73,15 +73,15 @@ class RecipeFilter {
             const recipeElement = document.createElement('li');
             recipeElement.className = 'recipe-item';
             recipeElement.innerHTML = `
-            <div class="flex flex-col place-content-around h-60 p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex flex-col place-content-around h-60 p-4 bg-gray-800 border border-gray-700 rounded-lg shadow-md shadow-gray-700">
                 <a class="self-center h-2/6" href="${recipe.slug}">
-                    <h5 class="recipe-name text-center mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">${recipe.name}</h5>
+                    <h5 class="recipe-name text-center mb-2 text-xl font-bold tracking-tight text-white">${recipe.name}</h5>
                 </a>
                 <ul class="flex flex-wrap justify-center content-between h-1/4">
-                    ${recipe.categories.map(category => `<li class="shrink-0 bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-400">${category.name}</li>`).join('')}
+                    ${recipe.categories.map(category => `<li class="shrink-0 me-2 px-2.5 py-0.5 text-xs font-medium rounded bg-gray-700 text-purple-400 border border-purple-400">${category.name}</li>`).join('')}
                 </ul>
-                <a href="${recipe.slug}" class="inline-flex items-center w-40 ml-auto px-3 py-2 text-sm font-medium text-center text-white bg-lime-600 rounded-lg hover:bg-lime-700 focus:ring-4 focus:outline-none focus:ring-lime-800 dark:bg-lime-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800">
-                    Aller à la recette
+                <a href="${recipe.slug}" class="inline-flex items-center w-32 ml-auto px-3 py-2 text-sm text-center text-white font-medium rounded-lg bg-lime-600 hover:bg-lime-700 focus:ring-4 focus:outline-none focus:ring-lime-800">
+                    Full recipe
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
