2a226155330389389013dd6c1e24258542c4b175
Chloé <chloerndt@gmail.com>
2024-08-29 22:59:08 +0200

    Added clean and save methods

--- mealplan/models/food.py
+++ mealplan/models/food.py
@@ -1,11 +1,30 @@
+"""
+Food model
+"""
+
 from django.db import models
+from django.core.exceptions import ValidationError
+
+import re
 
 
 class Food(models.Model):
-    """
-    Food model has a many-to-many relationship with recipe model.
-    """
+    """Food object"""
     name = models.CharField(max_length=100, unique=True)
+
+    def clean(self):
+        """Clean food name field before validation"""
+        if not self.name:
+            raise ValidationError('The name field cannot be empty.')
+        if not re.match(r'^[a-zA-Z\s]+$', self.name):
+            raise ValidationError('The name field must contain only letters and spaces.')
+        if Food.objects.filter(name__iexact=self.name).exists():
+            raise ValidationError('A food with this name already exists.')
+
+    def save(self, *args, **kwargs):
+        """Save Food object"""
+        self.clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name}"
