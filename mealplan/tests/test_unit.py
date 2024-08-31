c2eb7dc911ea4516ce5aee7afa5c73d4eb82eab9
Chloé <chloerndt@gmail.com>
2024-08-31 13:58:01 +0200

    Created Unit model tests

---
+++ mealplan/tests/test_unit.py
@@ -0,0 +1,58 @@
+"""
+Tests for Unit model
+"""
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from mealplan.models import Unit
+
+
+class UnitTests(TestCase):
+    """Test Unit model"""
+
+    def test_create_unit(self):
+        """Test creating a unit"""
+        unit = Unit.objects.create(name="Kilogram", short_name="kg", plural_name="Kilograms")
+        self.assertEqual(unit.name, "Kilogram")
+        self.assertEqual(unit.short_name, "kg")
+        self.assertEqual(unit.plural_name, "Kilograms")
+
+    def test_create_unit_with_empty_name(self):
+        """Test creating a unit with empty name"""
+        unit = Unit(name="")
+        with self.assertRaises(ValidationError):
+            unit.full_clean()
+            unit.save()
+
+    def test_create_unit_with_invalid_characters(self):
+        """Test creating a unit with invalid characters"""
+        invalid_names = ["12345", "@#$%", "Kilogram1", "Kilogram!"]
+        for name in invalid_names:
+            with self.assertRaises(ValidationError):
+                unit = Unit(name=name)
+                unit.full_clean()
+                unit.save()
+
+    def test_unit_name_max_length(self):
+        """Test that unit name cannot exceed max_length"""
+        max_length = Unit._meta.get_field('name').max_length
+        unit = Unit(name="A" * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            unit.full_clean()
+            unit.save()
+
+    def test_unit_short_name_max_length(self):
+        """Test that unit short name cannot exceed max_length"""
+        max_length = Unit._meta.get_field('short_name').max_length
+        unit = Unit(short_name="A" * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            unit.full_clean()
+            unit.save()
+
+    def test_unit_plural_name_max_length(self):
+        """Test that unit plural name cannot exceed max_length"""
+        max_length = Unit._meta.get_field('plural_name').max_length
+        unit = Unit(plural_name="A" * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            unit.full_clean()
+            unit.save()
