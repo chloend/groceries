33f102389f33eb923afc79215f3fe2ec899f6741
Chloé <chloerndt@gmail.com>
2024-09-13 16:52:11 +0200

    Reorganized project

--- app/api/urls.py
+++
@@ -1,9 +0,0 @@
-# api/urls.py
-
-from django.urls import path
-from .views import RecipeDetailsAPIView, RecipeListAPIView
-
-urlpatterns = [
-    path('recipes/<slug:slug>/', RecipeDetailsAPIView.as_view(), name='recipe-detail-api'),
-    path('recipes/', RecipeListAPIView.as_view(), name='recipe-list-api'),
-]
--- app/recipe/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .category import Category
-from .food import Food
-from .ingredient import Ingredient
-from .recipe import Recipe
-from .unit import Unit
