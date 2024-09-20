ed89c93af7989b60cc83e8fd5851baa6a522d4bf
Chloé <chloerndt@gmail.com>
2024-09-20 05:35:08 +0200

    DefaultButton component import

--- frontend/src/views/RecipeList.vue
+++ frontend/src/views/RecipeList.vue
@@ -4,12 +4,7 @@
         <ul id="recipe-list" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
             <li  v-for="recipe in recipes" :key="recipe.id" class="recipe-item">
                 <RecipeCard :title="recipe.name" :link="recipe.slug">
-                    <a v-bind:href="recipe.slug" class="inline-flex items-center w-32 ml-auto px-3 py-2 text-sm text-center text-white font-medium rounded-lg bg-lime-600 hover:bg-lime-700 focus:ring-4 focus:outline-none focus:ring-lime-800">
-                    Full recipe
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                    </a>
+                    <DefaultButton :link="recipe.slug">Full recipe</DefaultButton>
                 </RecipeCard>
             </li>
         </ul>
@@ -19,11 +14,13 @@
 <script>
 import axios from '@/axios';
 import RecipeCard from '@/components/RecipeCard.vue';
+import DefaultButton from '@/components/DefaultButton.vue';
 
 export default {
     name: 'RecipeList',
     components: {
-        RecipeCard
+        RecipeCard,
+        DefaultButton,
     },
     data() {
         return {
