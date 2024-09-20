b3fe1de02b95bcc755496f90445418adcce77f67
Chloé <chloerndt@gmail.com>
2024-09-20 04:56:33 +0200

    Added recipe slugs and deleted style tag

--- frontend/src/views/RecipeList.vue
+++ frontend/src/views/RecipeList.vue
@@ -1,9 +1,19 @@
 <template>
     <div>
-        <h1>Recipes</h1>
-        <ul id="recipe-list">
+        <h1 class="text-2xl sm:text-4xl font-bold pl-4 sm:pl-0">Recipes</h1>
+        <ul id="recipe-list" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
             <li  v-for="recipe in recipes" :key="recipe.id" class="recipe-item">
-                <h2 class="recipe-name">{{ recipe.name }}</h2>
+                <div class="flex flex-col place-content-around h-60 p-4 rounded-lg bg-gray-800 border border-gray-700 shadow-md shadow-gray-700">
+                    <a class="self-center h-2/6" v-bind:href="recipe.slug ">
+                        <h2 class="recipe-name text-center mb-2 text-xl font-bold tracking-tight text-white">{{ recipe.name }}</h2>
+                    </a>
+                    <a v-bind:href="recipe.slug" class="inline-flex items-center w-32 ml-auto px-3 py-2 text-sm text-center text-white font-medium rounded-lg bg-lime-600 hover:bg-lime-700 focus:ring-4 focus:outline-none focus:ring-lime-800">
+                    Full recipe
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
+                </div>
             </li>
         </ul>
     </div>
@@ -33,11 +43,3 @@ export default {
     }
 }
 </script>
-
-<style>
-h1,
-.recipe-name {
-    color: white;
-    text-align: center;
-}
-</style>
